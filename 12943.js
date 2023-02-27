6d6fa626f9fdc865220d2ff27a78defb9e6d2e2a
Kopite22 <nulee1000@gmail.com>
2023-02-27 23:39:19 +0900

    230227

--- 12943.js
+++ 12943.js
@@ -1,19 +1,19 @@
 function solution(num) {
-  for (i = 0; i < 500; i++){
+  for (i = 0; i < 500; i++) {
     if (num == 1) {
-      return i
+      return i;
     } else {
       if (num % 2 == 0) {
-        num = num/2
+        num = num / 2;
       } else {
-        num = num*3+1
+        num = num * 3 + 1;
       }
     }
   }
-  return -1
+  return -1;
 }
 
-// function collatz(num) {
+// function solution(num) {
 //   var answer = 0;
 //   while (num != 1 && answer != 500) {
 //     num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
