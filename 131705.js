3ee9e6f5537fd286678bc906e38cb341c8cbe50e
Kopite22 <nulee1000@gmail.com>
2023-05-03 00:08:54 +0900

    230503

--- 131705.js
+++ 131705.js
@@ -2,8 +2,16 @@ const numArr = [-2, 3, 0, 2, -5];
 
 function solution(numArr) {
   let result = 0;
-  console.log(numArr.sort((a, b) => a - b));
+
+  const sum = (arr, idx) => {
+    if (arr.length === 3) {
+      result += arr.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
+    }
+    for (let i = idx; i < Number.length; i++) {
+      sum([...arr, number[i]], i + 1);
+    }
+  };
+  sum([], 0);
   return result;
 }
-
-console.log(solution(numArr));
