f3e453f6b1e7b0529526089fa7e154a5743c9be5
Kopite22 <nulee1000@gmail.com>
2023-12-12 09:30:01 +0900

    231212

--- 181882.js
+++ 181882.js
@@ -1,16 +1,3 @@
-function solution(arr) {
-  const temp = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= 50 && arr[i] % 2 === 0) {
-      temp.push(arr[i] / 2);
-    } else if (arr[i] < 50 && arr[i] % 2 === 1) {
-      temp.push(arr[i] * 2);
-    } else {
-      temp.push(arr[i]);
-    }
-  }
-  return temp;
-}
 const solution = (a) =>
   a.map((v) =>
     v >= 50 && v % 2 == 0 ? v / 2 : v < 50 && v % 2 == 1 ? v * 2 : v
