b2528cb6a2d3211941e156251198d0fd2ec6a67c
Kopite22 <nulee1000@gmail.com>
2024-01-18 09:39:02 +0900

    240118

---
+++ 181839.js
@@ -0,0 +1,9 @@
+const solution = (a, b) => {
+  const isOdd = (num) => num % 2 === 1;
+
+  return isOdd(a) && isOdd(b)
+    ? a ** 2 + b ** 2
+    : isOdd(a) || isOdd(b)
+    ? 2 * (a + b)
+    : Math.abs(a - b);
+};
