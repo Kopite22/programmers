f3dbcca50a9c3c36b2f94f0afff8a126301e6c56
Kopite22 <nulee1000@gmail.com>
2024-03-20 09:48:05 +0900

    240320

---
+++ 136798.js
@@ -0,0 +1,25 @@
+const solution = (number, limit, power) => {
+  let res = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let cnt = 0;
+
+    // 약수 개수 구하기
+    // 약수의 대칭성을 이용
+    for (let j = 1; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        if (i / j === j) cnt += 1;
+        else cnt += 2;
+      }
+      // 약수의 개수가 공격력 제한 수치 넘은 경우
+      if (cnt > limit) {
+        cnt = power;
+        break;
+      }
+    }
+
+    res += cnt;
+  }
+
+  return res;
+};
