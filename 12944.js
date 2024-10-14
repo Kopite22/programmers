b906b2b02d940a46b4925d44b2390ddafbc252f5
Kopite22 <nulee1000@gmail.com>
2024-10-15 00:01:28 +0900

    241015

--- 12944.js
+++ 12944.js
@@ -1,2 +1,2 @@
-const answer = (arr: number[]) =>
+const solution = (arr) =>
   arr.reduce((acc, cur) => (acc += cur), 0) / arr.length;
--- 87389.js
+++ 87389.js
@@ -1,4 +1,4 @@
-function solution(n: number) {
+function solution(n) {
   for (let i = 2; i <= n - 1; i++) {
     if (n % i === 1) return i;
   }
